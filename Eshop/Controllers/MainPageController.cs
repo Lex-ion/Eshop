428ad4c5c13038f332eaad496f9913e333b7aa18
Lex-ion <117462399+Lex-ion@users.noreply.github.com>
2025-03-02 20:15:11 +0100

    Minor changes + images

--- Eshop/Controllers/MainPageController.cs
+++ Eshop/Controllers/MainPageController.cs
@@ -1,6 +1,8 @@
 ﻿using Eshop.Database;
 using Eshop.Entities;
+using Eshop.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Eshop.Controllers
 {
@@ -13,15 +15,19 @@ namespace Eshop.Controllers
 			_context = context;
 		}
 
-		public IActionResult Index()
+		public IActionResult Index(MainPageModel? model = null)
 		{
-			return View(_context.Products.ToList()) ;
+			if (model!.Products == null)
+				model = new MainPageModel(_context.Products.ToList(), null);
+
+			return View(model);
 		}
+
 
 		public IActionResult AddToCart(int id)
 		{
-			Product? prod = _context.Products.Single(p=> p.Id ==id);
-			if(prod is not null)
+			Product? prod = _context.Products.Single(p => p.Id == id);
+			if (prod is not null)
 			{
 				TempData["Message"] = $"Do košíku přidáno: {prod.Name}";
 				TempData["MessageType"] = "success";
@@ -33,6 +39,63 @@ namespace Eshop.Controllers
 			}
 
 			return RedirectToAction("Index");
+		}
+
+		public IActionResult Search(MainPageModel model)
+		{
+			if (string.IsNullOrEmpty(model.SearchString))
+				return RedirectToAction("Index");
+
+			Dictionary<Product, int> searchItems = new Dictionary<Product, int>();
+			string[] searchParams = model.SearchString.ToLower().Split(' ');
+			StringComparison c = StringComparison.OrdinalIgnoreCase;
+
+			var prods = _context.Products.ToList();
+
+			foreach (string param in searchParams)
+			{
+				foreach (var item in prods.Where(p => p.Name.ToLower().Contains(param)))
+				{
+					if (searchItems.ContainsKey(item))
+						searchItems[item]++;
+					else
+						searchItems[item] = 1;
+				}
+				foreach (var item in prods.Where(p => p.Description.ToLower().Contains(param)))
+				{
+					if (searchItems.ContainsKey(item))
+						searchItems[item]++;
+					else
+						searchItems[item] = 1;
+				}
+				
+				foreach (var prod in prods)
+				{
+					foreach (var prodCats in prod.ProductCategories.Where(p => p.Category.Name.ToLower().Contains(param)))
+					{
+						if (searchItems.ContainsKey(prod))
+							searchItems[prod]++;
+						else
+							searchItems[prod] = 1;
+					}
+				}
+				foreach (var prod in prods)
+				{
+					foreach (var prodCats in prod.ProductCategories.Where(p => p.Category.Description?.ToLower().Contains(param) ?? false))
+					{
+						if (searchItems.ContainsKey(prod))
+							searchItems[prod]++;
+						else
+							searchItems[prod] = 1;
+					}
+				}
+			}
+
+			List<Product> sorted = searchItems.OrderByDescending(entry => entry.Value).Select(entry => entry.Key).ToList();
+			model.Products = sorted;
+
+			return View("Index", model);
+
 		}
 	}
 }
---
+++ Eshop/Models/MainPageModel.cs
@@ -0,0 +1,20 @@
+﻿using Eshop.Entities;
+
+namespace Eshop.Models
+{
+	public class MainPageModel
+	{
+		public List<Product> Products { get; set; }
+		public string? SearchString { get; set; }
+
+		public MainPageModel(List<Product> products, string? searchString)
+		{
+			Products = products;
+			SearchString = searchString;
+		}
+
+		public MainPageModel()
+		{
+		}
+	}
+}
