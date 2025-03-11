0177b1b9894b30825f4af4c0d6e27352b2a3a01b
Lex-ion <117462399+Lex-ion@users.noreply.github.com>
2025-03-11 16:58:03 +0100

    secret

--- Eshop/Controllers/AdminController.cs
+++ Eshop/Controllers/AdminController.cs
@@ -51,7 +51,3 @@ namespace Eshop.Controllers
 
 	}
 }
-/*
- Tyhle technologie jsou tak zbytečně moc reflexivní, tak proč to neudělat ještě víc zbytečně reflexivní. Proč něco někam přidávat ručně, když na to můžu mít reflexivní atribut, který ani nemusí fungovat. A to jen protože se mi jmenují dvě věci trochu jinak.
- 
- */
