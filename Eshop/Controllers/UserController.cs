ceb884ee0a9501b02ae0c3d03f7374ace6e38c2a
Lex-ion <117462399+Lex-ion@users.noreply.github.com>
2025-03-17 20:14:43 +0100

    User phase started

---
+++ Eshop/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Eshop.Database;
+using Eshop.Helpers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Eshop.Controllers
+{
+	public class UserController : BaseController
+	{
+		public UserController(DatabaseContext context) : base(context)
+		{
+		}
+
+		public IActionResult Index()
+		{var ui = UserInfoExtractorHelper.GetUserInfo(_context, HttpContext); ;
+			ViewBag.UserInfo = ui;
+			ViewBag.Orders = _context.Orders.Where(o=>o.AccountId == ui.Id).ToList();
+			return View();
+		}
+	}
+}
