0a4b6d4ef63581bdc1041771ca3bea127b11f115
Lex-ion <117462399+Lex-ion@users.noreply.github.com>
2025-03-18 20:08:31 +0100

    very simple cart

---
+++ Eshop/Controllers/CartController.cs
@@ -0,0 +1,22 @@
+﻿using Eshop.Database;
+using Eshop.Helpers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Eshop.Controllers
+{
+	public class CartController : BaseController
+
+
+	{
+		public CartController(DatabaseContext context) : base(context)
+		{
+		}
+
+		public IActionResult Index()
+		{
+			var cart=CartHelper.GetCart(HttpContext, _context);
+
+			return View(cart);
+		}
+	}
+}
