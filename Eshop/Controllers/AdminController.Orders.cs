039b392605d4a154093487528c728e244d320ab4
Lex-ion <urbantomas@L211PC03>
2025-03-20 11:39:49 +0100

    Almost done order updating

---
+++ Eshop/Controllers/AdminController.Orders.cs
@@ -0,0 +1,21 @@
+﻿using Eshop.Models;
+using Microsoft.AspNetCore.Mvc;
+namespace Eshop.Controllers
+{
+    public partial class AdminController
+    {
+        public IActionResult EditOrders(int id)
+        {
+            var order = _context.Orders.Find(id);
+            UpdateOrderModel model = new UpdateOrderModel(order.Id,order,_context.OrderStates.ToList(),order.OrderStateId);
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult EditOrders(UpdateOrderModel model)
+        {
+          // var order = _context.Orders.Find(id);
+          // UpdateOrderModel model = new UpdateOrderModel(order.Id, order, _context.OrderStates.ToList(), order.OrderStateId);
+            return View(model);
+        }
+    }
+}
---
+++ Eshop/Models/UpdateOrderModel.cs
@@ -0,0 +1,24 @@
+﻿using Eshop.Entities;
+
+namespace Eshop.Models
+{
+    public class UpdateOrderModel
+    {
+        public int OrderID { get; set; }
+        public Order Order { get; set; }    
+        public List<OrderState> OrderStates { get; set; }
+        public int OrderStateId { get; set; }
+
+        public UpdateOrderModel()
+        {
+        }
+
+        public UpdateOrderModel(int orderID, Order order, List<OrderState> orderStates, int orderStateId)
+        {
+            OrderID = orderID;
+            Order = order;
+            OrderStates = orderStates;
+            OrderStateId = orderStateId;
+        }
+    }
+}
